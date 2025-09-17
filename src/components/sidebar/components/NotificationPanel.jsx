53d673a4ebecae9991a22810ec4b8c5a8473d40a
prajwalstulyfe <72973991+prajwaltikhe@users.noreply.github.com>
2025-09-17 17:10:55 +0530

    Sidebar added, Gitignore updated

---
+++ src/components/sidebar/components/NotificationPanel.jsx
@@ -0,0 +1,116 @@
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import { AppBar, Box, Tab, Tabs, Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import { Link } from "react-router-dom";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <div>{children}</div>
+          {/* <Typography>{children}</Typography> */}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
+}
+
+function NotificationPanel() {
+  const [value, setValue] = useState(0);
+  const theme = useTheme();
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <>
+      <div className="sub-menu notification-sub-menu">
+        <div
+          style={{
+            width: "23rem",
+            height: "calc(100vh - 100px)",
+            top: "-4rem",
+            left: "3.7rem",
+          }}
+        >
+          <AppBar
+            elevation={0}
+            sx={{ bgcolor: "transparent" }}
+            color="default"
+            position="static"
+          >
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="secondary"
+              textColor="inherit"
+              variant="fullWidth"
+              aria-label="Notification Tabs"
+            >
+              <Tab
+                sx={{
+                  bgcolor: value === 0 ? "#1d31a6" : "#07163d",
+                  color: "#FFF",
+                }}
+                label="Emergency Alerts"
+                {...a11yProps(0)}
+              />
+              <Tab
+                sx={{
+                  bgcolor: value === 1 ? "#1d31a6" : "#07163d",
+                  color: "#FFF",
+                }}
+                label="Geofence Violations"
+                {...a11yProps(1)}
+              />
+            </Tabs>
+          </AppBar>
+          {/* Panels */}
+          <TabPanel
+            className="text-black"
+            value={value}
+            index={0}
+            dir={theme.direction}
+          >
+            <div className="text-end">
+              <Link to="/management/emergency-alerts">
+                {/* <Link to="/management/emergency-alerts" onClick={toggleMenu}> */}
+                <button className="text-white">View All</button>
+              </Link>
+            </div>
+          </TabPanel>
+
+          <TabPanel value={value} index={1} dir={theme.direction}>
+            <div className="text-end">
+              <Link to="/management/emergency-alerts">
+                {/* <Link to="/management/emergency-alerts" onClick={toggleMenu}> */}
+                <button className="text-white">View All</button>
+              </Link>
+            </div>
+          </TabPanel>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NotificationPanel;
